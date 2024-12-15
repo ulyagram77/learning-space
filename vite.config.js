39785d5fcfd42dbee136c2ec9457ef35e4e58e32
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-12-16 00:51:18 +0200

    removed useless plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import includeHtml from 'vite-include-html-plugin';
 import { defineConfig } from 'vite';
 import path from 'node:path';
 
 export default defineConfig({
-  plugins: [includeHtml()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
