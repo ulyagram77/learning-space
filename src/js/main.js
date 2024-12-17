1b1ab2e4496d22b11c5e2348775bc335b4ad949e
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-12-17 22:55:47 +0200

    adapted hero section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import '../scss/style.scss';
 import { register } from 'swiper/element/bundle';
+import { useDynamicAdapt } from './dynamic-adapt';
 
 const burgerButton = document.querySelector('.header__burger-button');
 const menu = document.querySelector('.menu');
@@ -8,10 +9,12 @@ const menuLinks = document.querySelectorAll('.menu__link');
 document.addEventListener('DOMContentLoaded', () => {
   //init swiper-slider
   register();
+  useDynamicAdapt('max');
 
   burgerButton.addEventListener('click', () => {
     menu.classList.toggle('menu_active');
     burgerButton.classList.toggle('header__burger-button_active');
+    document.body.classList.toggle('no-scroll');
   });
 
   menuLinks.forEach(link => {
