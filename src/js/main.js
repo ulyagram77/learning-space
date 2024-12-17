0428b9842e3b1a875b08c3e0f898c26345ebbbe6
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-12-17 13:06:30 +0200

    created hero section for desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import '../scss/style.scss';
+import { register } from 'swiper/element/bundle';
 
 const burgerButton = document.querySelector('.header__burger-button');
 const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.menu__link');
 
 document.addEventListener('DOMContentLoaded', () => {
+  //init swiper-slider
+  register();
+
   burgerButton.addEventListener('click', () => {
     menu.classList.toggle('menu_active');
     burgerButton.classList.toggle('header__burger-button_active');
